3128b838b28ac8bd3dd4eacf5e5ca760be90eb25
PiNail <hendo420@super42.33mail.com>
2015-12-05 14:40:46 -0700

    Update oven.py
    SSR at NC

--- lib/oven.py
+++ lib/oven.py
@@ -146,11 +146,11 @@ class Oven (threading.Thread):
         if value:
             self.heat = 1.0
             if gpio_available:
-                GPIO.output(config.gpio_heat, GPIO.HIGH)
+                GPIO.output(config.gpio_heat, GPIO.LOW)
         else:
             self.heat = 0.0
             if gpio_available:
-                GPIO.output(config.gpio_heat, GPIO.LOW)
+                GPIO.output(config.gpio_heat, GPIO.HIGH)
 
     def set_cool(self, value):
         if value:
